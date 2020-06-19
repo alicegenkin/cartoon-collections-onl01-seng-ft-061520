05666dfd64184c732655a739acdf5ddbd2d552de
Alice <agenkin1@gmail.com>
2020-06-19 19:57:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,18 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
+    puts array.each_with_index {|val,index| puts "#{index+1}. #{val}"}
+  end
   # Your code here
+def summon_captain_planet(array)
+  return array.map {|name| name.capitalize.concat("!")}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.each do |string|
+    if string.length < 4
+      return false
+    else return true
+    end
+  end
 end
 
 def find_the_cheese# code an argument here
